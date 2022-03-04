b38c6f66b37129d13d5fb0efad315a06942bdc61
Ryan Duffy <ryan@replay.io>
2022-03-04 13:31:32 -0800

    Use cli to install browsers (#1)

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,10 @@
-import path from "path";
-
-type BrowserName = 'chromium' | 'firefox' | 'webkit';
-
-const EXECUTABLE_PATHS = {
-  "darwin:firefox": ["firefox", "Nightly.app", "Contents", "MacOS", "firefox"],
-  "linux:chromium": ["chrome-linux", "chrome"],
-  "linux:firefox": ["firefox", "firefox"],
-} as const;
-
-function getPlatformKey(browserName: BrowserName): keyof typeof EXECUTABLE_PATHS | undefined {
-  const key = `${process.platform}:${browserName}`;
-  switch (key) {
-    case "darwin:firefox":
-    case "linux:firefox":
-    case "linux:chromium":
-      return key;
-  }
-
-  return undefined;
-}
-
-export function getExecutablePath(browserName: BrowserName) {
-  // Override with replay specific browsers.
-  const replayDir =
-    process.env.RECORD_REPLAY_DIRECTORY ||
-    path.join(process.env.HOME!, ".replay");
-
-  const key = getPlatformKey(browserName);
-  if (!key) {
-    return;
-  }
-
-  return path.join(replayDir, "playwright", ...EXECUTABLE_PATHS[key]);
-}
+import {
+  getPlaywrightBrowserPath,
+  BrowserName,
+} from "@recordreplay/recordings-cli";
 
 function getDeviceConfig(browserName: BrowserName) {
-  const executablePath = getExecutablePath(browserName);
+  const executablePath = getPlaywrightBrowserPath(browserName);
   if (!executablePath) {
     console.warn(`${browserName} is not supported on this platform`);
   }
@@ -44,11 +13,15 @@ function getDeviceConfig(browserName: BrowserName) {
     launchOptions: {
       executablePath,
       env: {
-        RECORD_ALL_CONTENT: 1
+        RECORD_ALL_CONTENT: 1,
       },
     },
     defaultBrowserType: browserName,
   };
+}
+
+export function getExecutablePath(browserName: BrowserName) {
+  return getPlaywrightBrowserPath(browserName);
 }
 
 export const devices = {
